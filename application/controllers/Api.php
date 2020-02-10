10b98301c43b1ab1fcdf5d436ac2456a80a13b75
dev <dev@develop.com>
2020-02-11 05:27:39 +0530

    changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -7,6 +7,11 @@ class Api extends REST_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->API_ACCESS_KEY = 'AIzaSyBlRI5PaIZ6FJPwOdy0-hc8bTiLF5Lm0FQ';
+        // (iOS) Private key's passphrase.
+        $this->passphrase = 'joashp';
+        // (Windows Phone 8) The name of our push channel.
+        $this->channelName = "joashp";
         $this->load->model('Product_model');
         $this->load->library('session');
         $this->checklogin = $this->session->userdata('logged_in');
@@ -15,6 +20,115 @@ class Api extends REST_Controller {
 
     public function index() {
         $this->load->view('welcome_message');
+    }
+
+    private function useCurl($url, $headers, $fields = null) {
+        // Open connection
+        $ch = curl_init();
+        if ($url) {
+            // Set the url, number of POST vars, POST data
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            // Disabling SSL Certificate support temporarly
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            if ($fields) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+            }
+
+            // Execute post
+            $result = curl_exec($ch);
+            if ($result === FALSE) {
+                die('Curl failed: ' . curl_error($ch));
+            }
+
+            // Close connection
+            curl_close($ch);
+
+            return $result;
+        }
+    }
+
+    public function android($data, $reg_id_array) {
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $message = array(
+            'title' => $data['title'],
+            'message' => $data['message'],
+            'subtitle' => '',
+            'tickerText' => '',
+            'msgcnt' => 1,
+            'vibrate' => 1
+        );
+
+        $headers = array(
+            'Authorization: key=' . $this->API_ACCESS_KEY,
+            'Content-Type: application/json'
+        );
+
+        $fields = array(
+            'registration_ids' => $reg_id_array,
+            'data' => $message,
+        );
+
+        return $this->useCurl($url, $headers, json_encode($fields));
+    }
+
+    public function iOS($data, $devicetoken) {
+        $deviceToken = $devicetoken;
+        $ctx = stream_context_create();
+        // ck.pem is your certificate file
+        stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $this->passphrase);
+        // Open a connection to the APNS server
+        $fp = stream_socket_client(
+                'ssl://gateway.sandbox.push.apple.com:2195', $err,
+                $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+        if (!$fp)
+            exit("Failed to connect: $err $errstr" . PHP_EOL);
+        // Create the payload body
+        $body['aps'] = array(
+            'alert' => array(
+                'title' => $data['mtitle'],
+                'body' => $data['mdesc'],
+            ),
+            'sound' => 'default'
+        );
+        // Encode the payload as JSON
+        $payload = json_encode($body);
+        // Build the binary notification
+        $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+        // Send it to the server
+        $result = fwrite($fp, $msg, strlen($msg));
+
+        // Close the connection to the server
+        fclose($fp);
+        if (!$result)
+            return 'Message not delivered' . PHP_EOL;
+        else
+            return 'Message successfully delivered' . PHP_EOL;
+    }
+
+    function broadCastMessge($messagedict) {
+        $query = $this->db->get('gcm_registration');
+        $regarray2 = $query->result_array();
+        $temparray = [];
+        foreach ($regarray2 as $key => $value) {
+            array_push($temparray, $value['reg_id']);
+        }
+        $this->android($messagedict, $temparray);
+    }
+
+    function singleMessage($messagedict, $userid) {
+        $this->db->where('user_id', $userid);
+        $query = $this->db->get('gcm_registration');
+        $regarray2 = $query->result_array();
+        $temparray = [];
+        foreach ($regarray2 as $key => $value) {
+            array_push($temparray, $value['reg_id']);
+        }
+        $this->android($messagedict, $temparray);
     }
 
     function updateCurd_post() {
@@ -95,11 +209,11 @@ class Api extends REST_Controller {
         $query = $this->db->get('gcm_registration');
         $regarray2 = $query->result_array();
         if ($regarray2) {
-             $this->response(array("status" => "already", "data"=>$regArray));
+            $this->response(array("status" => "already", "data" => $regArray));
         } else {
             $this->db->insert('gcm_registration', $regArray);
         }
-        $this->response(array("status" => "done", "data"=>$regArray));
+        $this->response(array("status" => "done", "data" => $regArray));
     }
 
     function getUserCard_get($userid) {
@@ -461,8 +575,14 @@ class Api extends REST_Controller {
             'user_id' => $this->post('user_id'),
             "point_type" => $this->post('point_type'),
         );
+        $userid = $this->post('user_id');
         $this->db->insert('points', $class_assignment);
         $last_id = $this->db->insert_id();
+        $points =  $this->post('points');
+        $ctype = $this->post('point_type');
+        $cctype = $ctype == 'Credit'?'Credited':'Debited';
+        $message = array("title" => "Loyalty Card Points $cctype", "message" => "Your card has $cctype with $points points");
+        $this->singleMessage($message, $userid);
         $this->response(array("last_id" => $last_id));
     }
 
